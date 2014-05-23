d76c4da4ecf0a8abe22e42c48d1f1913d0b98526
Amir Behnam <abehnam@uwaterloo.ca>
2014-05-22 21:33:07 -0400

    b

--- 1/client.c
+++ 1/client.c
@@ -3,19 +3,19 @@
 #include "ece454rpc_types.h"
 
 int main() {
-//	int a = -10, b = 20;
-//        
-//	return_type ans = make_remote_call("localhost",
-//                                            10000,
-//                                            "addtwo", 2,
-//                                            sizeof(int), (void *)(&a),
-//                                            sizeof(int), (void *)(&b));
-//        printf("got it\n");
-//	int i = *(int *)(ans.return_val);
-//        printf("got it\n");
-//	printf("client, got result: %d\n", i);
+	int a = -10, b = 20;
         
+	return_type ans = make_remote_call("localhost",
+                                            10000,
+                                            "addtwo", 2,
+                                            sizeof(int), (void *)(&a),
+                                            sizeof(int), (void *)(&b));
+        printf("got it\n");
+	int i = *(int *)(ans.return_val);
+        printf("got it\n");
+	printf("client, got result: %d\n", i);
         
+        /*
         char * c = "asd";
         char * d = "fgh";
         return_type dd = make_remote_call("localhost",
@@ -25,6 +25,6 @@ int main() {
                                             strlen(d),(d));
 	char * e = (char *)(dd.return_val);
 	printf("client, got result: %s\n", e);
-
+         */
 	return 0;
 }
