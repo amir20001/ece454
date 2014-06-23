bc11b6ea457c56cbf8cc5323996aaa57c90d1200
Andrew Svoboda <svoboda.andrew@gmail.com>
2014-06-23 14:45:53 -0400

    try nop function

--- 1/client.c
+++ 1/client.c
@@ -6,17 +6,23 @@ int main() {
 	int a = 10, b = 20;
         
 	return_type ans = make_remote_call("localhost",
-                                            10000,
+                                            10007,
                                             "addtwo", 2,
                                             sizeof(int), (void *)(&a),
                                             sizeof(int), (void *)(&b));
 	int i = *(int *)(ans.return_val);
 	printf("client, got result: %d\n", i);
+
+	return_type aaa = make_remote_call("localhost",
+                                            10007,
+                                            "nop", 0);
+	i = *(int *)(aaa.return_val);
+	printf("client, got result: %d\n", i);
         
         char *c = "Hello work!";
         char *d = "This sentence has 6 words";
         return_type dd = make_remote_call("localhost",
-                                            10000,
+                                            10007,
                                             "concat", 2,
                                             strlen(c)+1,((void*)c),
                                             strlen(d)+1,((void*)d));
